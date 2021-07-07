8c8fa21fcdb5c69592564073222db8670fa77971
Alexis1226 <ahwr41@gmail.com>
2021-07-08 03:18:14 +0900

    local storage update

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,47 +3,42 @@ import PropTypes, { nominalTypeHack } from 'prop-types';
 import { RiStarLine, RiStarSmileFill } from 'react-icons/ri';
 import './Movie.css';
 
-function Movie({ id, year, title, summary, poster, genres, selected }) {
-  const [select, setSelect] = useState([]);
-  // setSelect(select.push(selected));
-  console.log(select);
-  // setSelect(...select, (select[index] = selected.toString()));
-
-  function onClick(e) {
-    selected = 'true';
-    setSelect('true');
-    console.log(e);
-    console.log(title, selected);
-  }
-
+function Movie({
+  index,
+  year,
+  title,
+  summary,
+  poster,
+  genres,
+  selected,
+  changeSelect,
+}) {
   return (
     <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <form>
-        <button type="submit" onClick={onClick}>
-          {select ? (
-            <RiStarSmileFill className="click" style={{ fill: 'gold' }} />
-          ) : (
-            <RiStarLine className="non-click" style={{ fill: '#cfcfcf' }} />
-          )}
-        </button>
-        <div className="movie__data">
-          <h3 className="movie__title">{title}</h3>
-          <h5 className="movie__year">{year}</h5>
-          <ul className="movie__genres">
-            {genres
-              .filter((e, i) => i < 4)
-              .map((genre, index) => (
-                <li key={index} className="genres__genre">
-                  {genre}
-                </li>
+      <button onClick={() => changeSelect(index)}>
+        {selected === 'true' ? (
+          <RiStarSmileFill className="click" style={{ fill: 'gold' }} />
+        ) : (
+          <RiStarLine className="non-click" style={{ fill: '#cfcfcf' }} />
+        )}
+      </button>
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres
+            .filter((e, i) => i < 4)
+            .map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
 
-                // map의 경우 반드시 key값이 필요하기 때문에 map에 자동으로 전해지는 index이용
-              ))}
-          </ul>
-          <p className="movie__summary">{summary.slice(0, 110)}...</p>
-        </div>
-      </form>
+              // map의 경우 반드시 key값이 필요하기 때문에 map에 자동으로 전해지는 index이용
+            ))}
+        </ul>
+        <p className="movie__summary">{summary.slice(0, 110)}...</p>
+      </div>
     </div>
   );
 }
--- src/pages/Contents.js
+++ src/pages/Contents.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Movie from '../components/Movie';
 
 function Contents() {
+  // API GET요청
   const [state, setState] = useState({
     isLoading: true,
     movies: [],
@@ -28,15 +29,20 @@ function Contents() {
     getMovies();
   }, []);
 
+  // LocalStorage로부터 data 받기
   const [TheMovies, setTheMovies] = useState([]);
   const localMovies = JSON.parse(localStorage.getItem('movies'));
 
-  // console.log(localMovies);
   useEffect(() => {
     setTheMovies(localMovies);
     console.log('set완료');
     return setTheMovies();
   }, [localMovies]);
+
+  function changeSelect(key) {
+    localMovies[key].select = 'true';
+    localStorage.setItem('movies', JSON.stringify(localMovies));
+  }
 
   return (
     <div id="contents">
@@ -50,13 +56,14 @@ function Contents() {
             return (
               <Movie
                 key={i}
-                id={movie.id}
+                index={i}
                 year={movie.year}
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
                 genres={movie.genres}
                 selected={movie.select}
+                changeSelect={changeSelect}
               />
             );
           })}
